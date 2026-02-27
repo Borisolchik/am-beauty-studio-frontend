f6b101add734de2e7b8ffa034342fc465ec531ac
Olga Boris <olya_boris97@mail.ru>
2026-02-27 16:26:02 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import IMask from 'imask'
 
-createApp(App)
-  .use(router)
-  .mount('#app')
+const app = createApp(App)
+
+app.directive('mask', {
+  mounted(el, binding) {
+    IMask(el, { mask: binding.value })
+  },
+})
+
+app.mount('#app')
